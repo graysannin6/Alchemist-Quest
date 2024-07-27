57b5439c922c35a94e9979465c4cac276ce4f9a1
Balsero <78754605+Balsero@users.noreply.github.com>
2024-07-27 04:48:03 -0400

    Craft window almost done

--- Assets/Scripts/UI/UI_CraftSlot.cs
+++ Assets/Scripts/UI/UI_CraftSlot.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UI_CraftSlot : UI_ItemSlot
-{
-    private void OnEnable()
+{   
+    
+    protected override void Start()
     {
-        UpdateSlot(item);
+        base.Start();
+    }
+
+    public void SetupCraftSlot(ItemData_Equipment _data)
+    {   
+        if(_data == null)
+        {
+            return;
+        }
+        item.data = _data;
+        itemImage.sprite = _data.icon;
+        itemText.text = _data.itemName;
     }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
-        ItemData_Equipment craftData = item.data as ItemData_Equipment;
-
-        Inventory.instance.CanCraft(craftData,craftData.craftingMaterials);
+        ui.craftWindow.SetupCraftWindow(item.data as ItemData_Equipment);
         
     }
     
--- Assets/Scripts/UI/UI_ItemSlot.cs
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -5,15 +5,15 @@ using UnityEngine.EventSystems;
 
 public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {   
-    [SerializeField]private Image itemImage;
-    [SerializeField]private TextMeshProUGUI itemText;
+    [SerializeField]protected Image itemImage;
+    [SerializeField]protected TextMeshProUGUI itemText;
 
-    private UI ui;
+    protected UI ui;
     public InventoryItem item;
 
-    public int descriptionLength = 50;
+    
 
-    private void Start()
+    protected virtual void Start()
     {
         ui = GetComponentInParent<UI>();
     }
@@ -52,6 +52,8 @@ public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler, IPointerEnterHand
         {
             Inventory.instance.EquipItem(item.data);
         }
+
+        ui.itemToolTip.HideToolTip();
 
     }
 
